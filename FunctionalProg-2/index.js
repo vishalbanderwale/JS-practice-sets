a70710bd97bde1bd53d7181f7fbf5f140de1154a
vishalbanderwale <vishalsonu945@gmail.com>
2024-10-26 15:11:52 +0530

    functional-2

--- FunctionalProg-2/index.js
+++ FunctionalProg-2/index.js
@@ -87,3 +87,124 @@ const productData = [
 const getProducts = (arr, b) => arr.filter(({ price }) => price > b);
 console.log(getProducts(productData, 40));
 // Output: [{prodName: 'Dairy Milk Silk', price: 70}, {prodName: 'Mars', price: 50}]
+
+// Write an ES6 function that takes an array of numbers and
+//returns the first number that is divisible by 7.
+
+const numbers3 = [1, 2, 3, 21, 14, 7];
+// Your code here
+
+const isDivisibleBy7 = numbers3.find((a) => a % 7 === 0);
+console.log(isDivisibleBy7);
+// Output: 21
+
+//Write an ES6 function that takes an array of strings and
+// returns the first string that is longer than 8 characters.
+
+const names1 = [
+  "Mohan",
+  "Anjali",
+  "Geetanjali",
+  "Ankit",
+  "Bhanu Kumar",
+  "Ramakrishnan",
+  "shayam",
+];
+// Your code here
+
+const isNamesGreaterThan8 = (arr) => arr.find((a) => a.length > 8);
+console.log(isNamesGreaterThan8(names1));
+// Output: "Geetanjali"
+
+//Write an ES6 function that takes an array of objects representing students
+// with properties name and grade.
+//Return the first student object that has a grade of "A".
+
+const students = [
+  { name: "John", grade: "B" },
+  { name: "Mary", grade: "A" },
+  { name: "Sam", grade: "C" },
+  { name: "Sarah", grade: "A" },
+];
+
+// Your code here
+
+const findStudentWithGradeA = (arr) => arr.find(({ grade }) => grade === "A");
+
+const studentWithGradeA = findStudentWithGradeA(students);
+console.log(studentWithGradeA);
+// Output: { name: "Mary", grade: "A" }
+
+//Write an ES6 function that takes an array of objects representing
+// students with properties name, grade and scholarship.
+//Return the first student object that has a grade of "A" or they are a scholarship student.
+
+const students1 = [
+  { name: "John", grade: "B", scholarship: false },
+  { name: "Mary", grade: "B", scholarship: true },
+  { name: "Sam", grade: "A", scholarship: false },
+  { name: "Sarah", grade: "A", scholarship: true },
+];
+
+// Your code here
+
+const findStudent = (arr) =>
+  arr.find(({ grade, scholarship }) => grade === "A" || scholarship === true);
+
+const student = findStudent(students1);
+console.log(student);
+// Output: { name: "Mary", grade: "B", scholarship: true }
+
+// Write an ES6 function that takes an array of objects representing
+// students with properties name and grade.
+// Return the first student object that has a grade of "B" and they are also a scholarship student.
+
+const students7 = [
+  { name: "John", grade: "B", scholarship: false },
+  { name: "Mary", grade: "A", scholarship: true },
+  { name: "Sam", grade: "A", scholarship: false },
+  { name: "Sarah", grade: "B", scholarship: true },
+];
+
+// Your code here
+
+const findStudent1 = (arr) =>
+  arr.find(({ grade, scholarship }) => grade === "B" && scholarship === true);
+
+const student5 = findStudent1(students7);
+console.log(student5);
+// Output: { name: "Sarah", grade: "B", scholarship: true }
+
+//Write an ES6 function that takes an array of objects containing
+// Bollywood movie information (title, director, year, rating) and returns an array with
+//  only the movie titles that were made before 1990 and has a rating above 8.0. (Question Level: tough)
+
+const bollywoodMovies = [
+  { title: "Sholay", director: "Ramesh Sippy", year: 1975, rating: 8.2 },
+  {
+    title: "Amar Akbar Anthony",
+    director: "Manmohan Desai",
+    year: 1977,
+    rating: 7.6,
+  },
+  { title: "Namak Halaal", director: "Prakash Mehra", year: 1982, rating: 7.4 },
+  { title: "Mr. India", director: "Shekhar Kapur", year: 1987, rating: 7.8 },
+  {
+    title: "Qayamat Se Qayamat Tak",
+    director: "Mansoor Khan",
+    year: 1988,
+    rating: 7.6,
+  },
+  { title: "Parinda", director: "Vidhu Vinod Chopra", year: 1989, rating: 8.1 },
+  { title: "Dil", director: "Indra Kumar", year: 1990, rating: 7.8 },
+];
+
+// Your code here
+
+const getBestOldBollywoodMovies = (arr) =>
+  arr
+    .filter(({ year, rating }) => year < 1990 && rating > 8)
+    .map(({ title }) => title);
+
+const bestOldMovies = getBestOldBollywoodMovies(bollywoodMovies);
+console.log(bestOldMovies); // Output: ['Sholay', 'Parinda']
